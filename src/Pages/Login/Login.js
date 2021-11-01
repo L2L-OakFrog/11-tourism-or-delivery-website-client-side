918bbf0090d0335fd364c936fe14107d1a85df30
L2L_OakFrog <rafiqahamed.tanim@gmail.com>
2021-11-01 20:34:17 +0600

    login edit

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { useHistory, useLocation } from 'react-router';
 import useAuth from '../../Hooks/UseAuth';
 
 const Login = () => {
-    const { login, error, signInUsingGoogle, handleName, handleEmail, handlePassword, handleLogin, handleRegister } = useAuth();
+    const { signInUsingGoogle } = useAuth();
     const location = useLocation();
     const history = useHistory();
     const redirect_url = location.state?.from || '/home';
@@ -16,11 +16,21 @@ const Login = () => {
             })
     }
     return (
-        <div>
-            <Button onClick={handleGoogle} variant="primary" type="submit">
-                Google Login
-            </Button>
-        </div>
+        <Container>
+            <Row className='showinglogin'>
+                <Col>
+                    <h1>Login with Google</h1>
+                    <br />
+                    <Button onClick={handleGoogle} variant="primary" type="submit">
+                        Google Login
+                    </Button>
+                </Col>
+                <Col>
+                    <img className="w-75" src="https://image.freepik.com/free-vector/sign-concept-illustration_114360-5375.jpg" alt="" />
+                </Col>
+            </Row>
+
+        </Container>
     );
 };
 
